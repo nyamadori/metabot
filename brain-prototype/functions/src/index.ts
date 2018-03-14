d12a25a7c1c02bd870dc343e4ae41968946cb04b
nyamadori <nyamadorig@gmail.com>
2018-03-15 01:02:28 +0900

    Experimental: Add database writing in the function

--- brain-prototype/functions/src/index.ts
+++ brain-prototype/functions/src/index.ts
@@ -1,8 +1,34 @@
-import * as functions from 'firebase-functions';
+import * as firebase from "firebase"
+require("firebase/firestore")
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/functions/write-firebase-functions
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+import * as functions from 'firebase-functions'
+
+const config = {
+  apiKey: "AIzaSyCrW0o6SZztAlE32m_nIARtwxuWfTCM8Jg",
+  authDomain: "metabot-187207.firebaseapp.com",
+  databaseURL: "https://metabot-187207.firebaseio.com",
+  projectId: "metabot-187207",
+  storageBucket: "metabot-187207.appspot.com",
+  messagingSenderId: "284964654585"
+}
+
+firebase.initializeApp(config)
+
+export const helloWorld = functions.https.onRequest((request, response) => {
+  const db = firebase.firestore()
+
+  db.collection("users").add({
+    first: "Alan",
+    middle: "Mathison",
+    last: "Turing",
+    born: 1912
+  })
+    .then(function (docRef) {
+      console.log("Document written with ID: ", docRef.id);
+    })
+    .catch(function (error) {
+      console.error("Error adding document: ", error);
+    })
+
+ response.send("Hello from Firebase!");
+});
