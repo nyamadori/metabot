0cf28a1443458665278d856534508fdd3dce5669
nyamadori <nyamadorig@gmail.com>
2018-03-24 22:06:06 +0900

    Add /exector/sessions API

--- metabot/functions/src/routes.ts
+++ metabot/functions/src/routes.ts
@@ -1,6 +1,8 @@
 import Router from 'express-promise-router'
 import * as slackCommandMessages from './apis/slack/command/messages'
+import * as exectorAuth from './apis/exector/sessions'
 
 export const router = Router()
 
 router.use('/slack/command/messages', slackCommandMessages.router)
+router.use('/exector/sessions', exectorAuth.router)
--- metabot/functions/src/utils/firebase.ts
+++ metabot/functions/src/utils/firebase.ts
@@ -1,7 +1,11 @@
 import * as functions from 'firebase-functions'
 import * as admin from 'firebase-admin'
+import serviceAccount from '../credentials/service-account'
 
-admin.initializeApp(functions.config().firebase)
+admin.initializeApp({
+  ...functions.config().firebase,
+  credential: admin.credential.cert(serviceAccount)
+})
 
 export const firestore = admin.firestore()
 export const auth = admin.auth()
